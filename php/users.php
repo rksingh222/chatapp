685357dfe9dc609676d23d9e730a8b532bb694e6
rahulsingh <rahul.singh.22.02.1985@gmail.com>
2023-03-10 03:14:00 +1100

    changes

--- php/users.php
+++ php/users.php
@@ -2,6 +2,7 @@
 session_start();
 include_once "config.php";
 $sql = mysqli_query($conn, "SELECT * FROM users");
+$outgoing_id = $_SESSION['unique_id'];
 $output = "";
 
 if(mysqli_num_rows($sql) == 1){
